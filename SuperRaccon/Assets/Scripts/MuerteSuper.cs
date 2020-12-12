2ca9c63edd649fc31efb741e627956ba85899f86
Rosa Morales <05rosa04@gmail.com>
2020-12-11 21:20:30 -0500

    New Game

---
+++ SuperRaccon/Assets/Scripts/MuerteSuper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MuerteSuper : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y > 5)
+            SceneManager.LoadScene(0);
+
+        if(transform.position.y < -5)
+            SceneManager.LoadScene(0);
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ SuperRaccon/Assets/Scripts/VueloSuper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VueloSuper : MonoBehaviour
+{
+    public Vector2 FuerzaSalto;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(FuerzaSalto);
+        }
+    }
+}
